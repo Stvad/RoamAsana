d08d89249c3120c9329d8483f981df5afd609b58
Vladyslav Sitalo <sitalov@amazon.com>
2020-01-05 14:54:19 -0800

    v2: Release Candidate

--- roam_asana.py
+++ roam_asana.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+import re
 from dataclasses import dataclass, field
 from pathlib import Path
 from pprint import pprint
@@ -33,10 +34,7 @@ def main():
     asana = json.load(input_path.open())
 
     root_page = Page(input_path.stem, convert_tasks(asana['data']))
-    # print(root_page)
     pprint(root_page.to_json())
-    # pprint(Page.schema().dump([root_page], many=True))
-    # Page.schema().dump([root_page], Path(argv[2]).open(mode="w"))
     Path(argv[2]).write_text(f"[{root_page.to_json()}]")
 
 
@@ -47,25 +45,50 @@ def roam_string(task_json):
     return result
 
 
+def process_task(task_json):
+    # maybe square bracket estiamate into the attribute?
+    return task_section(task_json), Block(roam_string(task_json), extract_children(task_json))
+
+
+def task_section(task_json, separator='|'):
+    """If tasks is in multiple Asana sections - combine them into an aggregate section connected by separator"""
+    return seq(task_json['memberships']).map(lambda it: it['section']['name']).make_string(separator)
+
+
+def get_bracket_number(task_name):
+    # support for bracket estimates: https://github.com/Stvad/Asana-counter
+    regex = '\[([-+]?(\d+|\d+\.\d+))]'
+    match = re.search(regex, task_name)
+    if match:
+        return match.group(1)
+    return None
+
+
+def extract_children(task_json):
+    def bracket_estimate(estimate_property="asana_pomodoro_estimate"):
+        bracket_number = get_bracket_number(task_json['name'])
+        return [Block(f"{estimate_property}::{bracket_number}")] if bracket_number else []
+
+    def notes():
+        return [Block(task_json['notes'])] if task_json['notes'] else []
+
+    def tags():
+        tag_str = seq(task_json['tags']).map(lambda it: f"[[{it['name']}]]").make_string(' ')
+        return [Block(tag_str)] if tag_str else []
+
+    return tags() + bracket_estimate() + notes() + convert_tasks(task_json['subtasks'])
+
+
 def convert_tasks(tasks):
-    def process_task(task_json):
-        # section as tag? or as a parent?
-        # maybe square bracket estiamate into the attribute?
-        return Block(roam_string(task_json), extract_children(task_json))
+    return (seq(tasks).map(process_task).group_by_key()
+            .flat_map(section_blocks).to_list())
 
-    def extract_children(task_json):
-        children = convert_tasks(task_json['subtasks'])
 
-        if task_json['notes']:
-            children.insert(0, Block(task_json['notes']))
-
-        if task_json['tags']:
-            tags = seq(task_json['tags']).map(lambda it: f"[[{it['name']}]]").make_string(' ')
-            children.insert(0, Block(tags))
-
-        return children
-
-    return seq(tasks).map(process_task).to_list()
+def section_blocks(block_pair):
+    section_name, blocks = block_pair
+    if (not section_name) or section_name == "(no section)":
+        return blocks
+    return [Block(section_name, blocks)]
 
 
 if __name__ == '__main__':
