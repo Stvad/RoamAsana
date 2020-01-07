3e0cf83866d1c83ffbf981308caf04507a02be6e
Vladyslav Sitalo <sitalov@amazon.com>
2020-01-06 19:53:14 -0800

    Extract the Task class

--- roam_asana.py
+++ roam_asana.py
@@ -28,39 +28,48 @@ class Page:
     children: List[Block]
 
 
-def main():
-    """Usage roam_asana asana_export.json roam.json"""
-    input_path = Path(argv[1])
-    asana = json.load(input_path.open())
+@dataclass
+class Task:
+    task_json: dict
 
-    root_page = Page(input_path.stem, convert_tasks(asana['data']))
-    json_str = root_page.to_json(indent=2)
-    print(json_str)
+    def as_block(self) -> Block:
+        return Block(self.roam_name(), self.children())
 
-    Path(argv[2]).write_text(f"[{json_str}]")
+    def name(self):
+        return self.task_json['name']
+
+    def roam_name(self):
+        return "{{[[DONE]]}} " + self.name() if self.task_json['completed'] else self.name()
+
+    def section(self, separator='|'):
+        """If tasks is in multiple Asana sections - combine them into an aggregate section connected by separator"""
+        return seq(self.task_json['memberships']).map(lambda it: it['section']['name']).make_string(separator)
+
+    def children(self):
+        def description():
+            notes = self.task_json['notes']
+            return [Block(notes)] if notes else []
+
+        def bracket_estimate(estimate_property="asana_pomodoro_estimate"):
+            bracket_number = get_bracket_number(self.name())
+            return [Block(f"{estimate_property}::{bracket_number}")] if bracket_number else []
+
+        def due_date_tag():
+            return [custom_strftime('%B {S}, %Y', datetime.fromisoformat(self.task_json['due_on']))] \
+                if self.task_json['due_on'] else []
+
+        def tags():
+            tag_names = seq(due_date_tag()) + seq(self.task_json['tags']).map(lambda it: it['name'])
+            tag_str = tag_names.map(lambda it: f"[[{it}]]").make_string(' ')
+            return [Block(tag_str)] if tag_str else []
+
+        return tags() + bracket_estimate() + description() + convert_tasks(self.task_json['subtasks'])
 
 
-def roam_string(task_json):
-    result = task_json['name']
-    if task_json['completed']:
-        result = "{{[[DONE]]}} " + result
-    return result
-
-
-def process_task(task_json):
-    # maybe square bracket estiamate into the attribute?
-    return task_section(task_json), Block(roam_string(task_json), extract_children(task_json))
-
-
-def task_section(task_json, separator='|'):
-    """If tasks is in multiple Asana sections - combine them into an aggregate section connected by separator"""
-    return seq(task_json['memberships']).map(lambda it: it['section']['name']).make_string(separator)
-
-
-def get_bracket_number(task_name):
+def get_bracket_number(string):
     # support for bracket estimates: https://github.com/Stvad/Asana-counter
     regex = '\[([-+]?(\d+|\d+\.\d+))]'
-    match = re.search(regex, task_name)
+    match = re.search(regex, string)
     if match:
         return match.group(1)
     return None
@@ -73,36 +82,33 @@ def custom_strftime(date_format, date):
     return date.strftime(date_format).replace('{S}', str(date.day) + suffix(date.day))
 
 
-def extract_children(task_json):
-    def bracket_estimate(estimate_property="asana_pomodoro_estimate"):
-        bracket_number = get_bracket_number(task_json['name'])
-        return [Block(f"{estimate_property}::{bracket_number}")] if bracket_number else []
-
-    def notes():
-        return [Block(task_json['notes'])] if task_json['notes'] else []
-
-    def due_date_tag():
-        return [custom_strftime('%B {S}, %Y', datetime.fromisoformat(task_json['due_on']))] \
-            if task_json['due_on'] else []
-
-    def tags():
-        tag_names = seq(due_date_tag()) + seq(task_json['tags']).map(lambda it: it['name'])
-        tag_str = tag_names.map(lambda it: f"[[{it}]]").make_string(' ')
-        return [Block(tag_str)] if tag_str else []
-
-    return tags() + bracket_estimate() + notes() + convert_tasks(task_json['subtasks'])
-
-
 def convert_tasks(tasks):
     return (seq(tasks).map(process_task).group_by_key()
-            .flat_map(section_blocks).to_list())
+            .flat_map(extract_sections).to_list())
 
 
-def section_blocks(block_pair):
+def process_task(task_json):
+    task = Task(task_json)
+    return task.section(), task.as_block()
+
+
+def extract_sections(block_pair):
     section_name, blocks = block_pair
     if (not section_name) or section_name == "(no section)":
         return blocks
     return [Block(section_name, blocks)]
+
+
+def main():
+    """Usage roam_asana asana_export.json roam.json"""
+    input_path = Path(argv[1])
+    asana = json.load(input_path.open())
+
+    root_page = Page(input_path.stem, convert_tasks(asana['data']))
+    json_str = root_page.to_json(indent=2)
+    print(json_str)
+
+    Path(argv[2]).write_text(f"[{json_str}]")
 
 
 if __name__ == '__main__':
